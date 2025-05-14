5e63173757e7ffd53655f33bd9cc27bd048899ad
fir36 <110829845+fir36@users.noreply.github.com>
2025-05-14 14:11:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,34 @@
-resource "aws_s3_bucket" "bad_bucket" {
-  bucket = "prisma-cloud-test-bucket"
-  acl    = "private"}
+resource "aws_s3_bucket" "good_bucket" {
+  bucket = "prisma-cloud-good-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = {
-    Name        = "PrismaCloudTestBucket"
+    Name        = "GoodBucket"
     Environment = "Test"
   }
 }
 
-
-resource "aws_s3_bucket" "bad_bucket_log_bucket" {
-  bucket = "bad_bucket-log-bucket"
-}
-
-resource "aws_s3_bucket_logging" "bad_bucket" {
-  bucket = aws_s3_bucket.bad_bucket.id
-
-  target_bucket = aws_s3_bucket.bad_bucket_log_bucket.id
-  target_prefix = "log/"
-}
-
-
-resource "aws_security_group" "bad_security_group" {
-  name        = "prisma-cloud-test-sg"
-  description = "Allow all inbound traffic"
+resource "aws_security_group" "good_sg" {
+  name        = "prisma-cloud-secure-sg"
+  description = "Allow HTTPS from trusted source"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # This is a misconfiguration (open to the world)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/24"] # Replace with valid CIDR
   }
 
   egress {
@@ -40,7 +39,7 @@ resource "aws_security_group" "bad_security_group" {
   }
 
   tags = {
-    Name        = "PrismaCloudTestSG"
+    Name        = "GoodSG"
     Environment = "Test"
   }
 }
