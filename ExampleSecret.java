30a99576bcaadb62fb42c01972f2d5d92c3f230e
labtestsubb <152941466+labtestsubb@users.noreply.github.com>
2025-05-22 12:31:39 +0800

    Update ExampleSecret.java

--- ExampleSecret.java
+++ ExampleSecret.java
@@ -2,7 +2,6 @@ public class ExampleSecret {
     public static void main(String[] args) {
         String awsSecretKey = "AKIAIOSFODNN7EXAMPLE"; // AWS Access Key (detected)
         String dbPassword = "SuperSecret123";         // Database password (detected)
-
         System.out.println("Secrets in code are bad!");
     }
 }
